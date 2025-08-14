2c5138ec2e6fba373f7d096db6a74d96e2824a87
Kendallfallassss <kendfb07@gmail.com>
2025-08-14 17:37:08 -0600

    Comit recuperacion de archivos

---
+++ CostaAzul.Web/Controllers/ClienteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CostaAzul.Web.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
---
+++ CostaAzul.Web/Controllers/OpinionesController.cs
@@ -0,0 +1,32 @@
+﻿using CostaAzul.API.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using CostaAzul.API.Models.Entities;
+
+[Authorize]
+public class OpinionesController : Controller
+{
+    private readonly CostaAzulContext _context;
+
+    public OpinionesController(CostaAzulContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Crear(Opinion opinion)
+    {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        if (userId == null) return Unauthorized();
+
+        opinion.UsuarioId = int.Parse(userId);
+        opinion.Fecha = DateTime.UtcNow;
+
+        _context.Opiniones.Add(opinion);
+        await _context.SaveChangesAsync();
+
+        return RedirectToAction("Details", "Hoteles", new { id = opinion.HotelId });
+    }
+}
